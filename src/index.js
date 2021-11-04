262476c50a29658bc70298957f001ab9bee79e30
Vít Svoboda <vit.svoboda@gmail.com>
2021-11-05 00:35:15 +0100

    Add an example of link customization to README

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import RichText from './components/rich-text';
 import linkedItemFactory from './components/linkedItemFactory';
+import linkFactory from './components/linkFactory';
 
 export {
   RichText,
   linkedItemFactory,
+  linkFactory,
 }
